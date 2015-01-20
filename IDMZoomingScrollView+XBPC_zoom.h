e0109e993362ac1e0e4195f39cfdca5ee40abe6c
eugenenguyen <xuanbinh91@gmail.com>
2015-01-20 15:46:31 +0700

    Release 0.3.3.2

---
+++ IDMZoomingScrollView+XBPC_zoom.h
@@ -0,0 +1,13 @@
+//
+//  IDMZoomingScrollView+XBPC_zoom.h
+//  Pods
+//
+//  Created by Binh Nguyen Xuan on 1/20/15.
+//
+//
+
+#import "IDMZoomingScrollView.h"
+
+@interface IDMZoomingScrollView (XBPC_zoom)
+
+@end
---
+++ IDMZoomingScrollView+XBPC_zoom.m
@@ -0,0 +1,55 @@
+//
+//  IDMZoomingScrollView+XBPC_zoom.m
+//  Pods
+//
+//  Created by Binh Nguyen Xuan on 1/20/15.
+//
+//
+
+#import "IDMZoomingScrollView+XBPC_zoom.h"
+
+@implementation IDMZoomingScrollView (XBPC_zoom)
+
+- (void)setMaxMinZoomScalesForCurrentBounds {
+    // Reset
+    self.maximumZoomScale = 1;
+    self.minimumZoomScale = 1;
+    self.zoomScale = 1;
+    
+    // Bail
+    if (self.photoImageView.image == nil) return;
+    
+    // Sizes
+    CGSize boundsSize = self.bounds.size;
+    CGSize imageSize = self.photoImageView.frame.size;
+    
+    // Calculate Min
+    CGFloat xScale = boundsSize.width / imageSize.width;    // the scale needed to perfectly fit the image width-wise
+    CGFloat yScale = boundsSize.height / imageSize.height;  // the scale needed to perfectly fit the image height-wise
+    CGFloat minScale = MIN(xScale, yScale);                 // use minimum of these to allow the image to become fully visible
+    
+    // If image is smaller than the screen then ensure we show it at
+    // min scale of 1
+    if (xScale > 1 && yScale > 1) {
+        //minScale = 1.0;
+    }
+    
+    // Calculate Max
+    CGFloat maxScale = 4.0; // Allow double scale
+    // on high resolution screens we have double the pixel density, so we will be seeing every pixel if we limit the
+    // maximum zoom scale to 0.5.
+    //	if ([UIScreen instancesRespondToSelector:@selector(scale)]) {
+    //		maxScale = maxScale / [[UIScreen mainScreen] scale];
+    //	}
+    
+    // Set
+    self.maximumZoomScale = maxScale;
+    self.minimumZoomScale = minScale;
+    self.zoomScale = minScale;
+    
+    // Reset position
+    self.photoImageView.frame = CGRectMake(0, 0, self.photoImageView.frame.size.width, self.photoImageView.frame.size.height);
+    [self setNeedsLayout];
+}
+
+@end
